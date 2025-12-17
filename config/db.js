7b7af683fff356b489055c118ac88adf4ba0c249
sivabharathi-k <sivabharathi.k2024it@sece.ac.in>
2025-12-17 15:55:58 +0530

    update

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose =require("mongoose")
 
 const connectDB=async()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("connected to database");
     }
     catch(err){
